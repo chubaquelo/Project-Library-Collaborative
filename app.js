0c7128c3fb6d32f2362bc19f3c9968a317a72fd2
Sergio <sergioobolevich@gmail.com>
2021-01-22 14:19:52 -0300

    Add btn just style, fix some errors.

--- app.js
+++ app.js
@@ -5,34 +5,10 @@ const pagesInput = document.querySelector("#pages-number");
 const readStatusInput = document.querySelector("#read-status");
 const buttonInput = document.querySelector(".btn");
 
-let myLibrary = [
-  {
-    title: "Book 1",
-    description: "Description 1",
-    pages: "950",
-    readStatus: false,
-  },
-  {
-    title: "Book 2",
-    description: "Description 2",
-    pages: "850",
-    readStatus: true,
-  },
-  {
-    title: "Book 3",
-    description: "Description 3",
-    pages: "750",
-    readStatus: false,
-  },
-  {
-    title: "Book 4",
-    description: "Description 5",
-    pages: "650",
-    readStatus: false,
-  },
-];
+let myLibrary = [];
 
 buttonInput.addEventListener("click", createNewBook);
+
 
 function createNewBook() {
   let title = bookTitleInput.value;
@@ -51,17 +27,20 @@ function Book(title, description, pages, read) {
 
 function addNewBook() {
   let addNew = document.querySelector(".new-book");
-  if (addNew.style.display === "none") {
-    addNew.style.display = "flex";
+  if (addNew.style.display === "flex") {
+    addNew.style.display = "none";
   } else {
-    addNew.style.display = "none"
+    addNew.style.display = "flex"
   }
 }
 
 function addBookToLibrary(book) {
   // do stuff here
+  myLibrary.push(book);
+
   let bookCard = document.createElement('div');
   bookCard.className = 'book-card';
+  bookCard.setAttribute('data-idx', myLibrary.indexOf(book));
 
   let bookTitle = document.createElement('h1');
   bookTitle.textContent = book.title;
@@ -75,14 +54,52 @@ function addBookToLibrary(book) {
   bookPages.className = 'pages';
 
   let readStatus = document.createElement('p');
-  if (book.readStatus === true) {
+  if (book.read === true) {
     readStatus.textContent = 'You have read this book.';
   } else {
     readStatus.textContent = 'You have not read this book.';
   }
 
-  bookCard.append(bookTitle, bookDescription, bookPages, readStatus);
+  let changeReadStatusBtn = document.createElement('button');
+  changeReadStatusBtn.className = 'read-switch-btn';
+  
+  if (book.read === true) {
+    changeReadStatusBtn.textContent = "Unread";
+  } else {
+    changeReadStatusBtn.textContent = "Read";
+  }
+  changeReadStatusBtn.addEventListener("click", switchReadStatus(book));
+
+  let removeBtn = document.createElement('button');
+  removeBtn.className = 'remove-btn';
+  removeBtn.textContent = "Remove Book";
+  removeBtn.addEventListener("click", removeCard);
+
+  bookCard.append(
+    bookTitle,
+    bookDescription,
+    bookPages,
+    readStatus,
+    changeReadStatusBtn, 
+    removeBtn
+  );
   booksContainer.appendChild(bookCard);
+}
+
+function removeCard(e) {
+  e.target.position();
+}
+
+function switchReadStatus(book) {
+  // switch button and read status for a book
+  // let readBtn = document.querySelector(".read-switch-btn");
+  // if (book.read === true) {
+  //   book.read = false;
+  //   readBtn.textContent = "Read";
+  // } else {
+  //   book.read = true;
+  //   readBtn.textContent = "Unread";
+  // }
 }
 
 myLibrary.forEach((book) => addBookToLibrary(book));
