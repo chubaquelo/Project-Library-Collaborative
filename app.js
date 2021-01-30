960c07fec2f7835711091d06c9ab7314c23618b7
Sergio <sergioobolevich@gmail.com>
2021-01-29 23:56:00 -0300

    Refactor app to modules pattern.

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-let myLibrary = [];
-
-function setMyLibrary() {
-  if (localStorage.getItem("myLibrary") !== null) {
-    myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
-    loadBooks();
-  } else {
-    myLibrary = [];
-  }
-}
-
+// Define DOM variables
 const booksContainer = document.querySelector(".books-container");
 const bookTitleInput = document.querySelector("#book-title");
 const bookAuthorInput = document.querySelector("#book-author");
@@ -18,147 +8,165 @@ const buttonInput = document.querySelector(".btn");
 const btnAddNewBook = document.querySelector(".btn-add-new-book");
 const newBookForm = document.querySelector(".new-book");
 
-buttonInput.addEventListener("click", createNewBook);
+// Define arrow function modules to be called later
 
-function createNewBook() {
-  if (bookTitleInput.value == "" || bookAuthorInput.value == "" || pagesInput.value ==""){
-    alert('You must complete all fields.');
-  } else {
-    let title = bookTitleInput.value;
-    let author = bookAuthorInput.value;
-    let pages = pagesInput.value;
-    let status = readStatusInput.checked;
+const mySecModule = (() => {
+  function addBookToLibrary(book) {
+    let bookCard = document.createElement("div");
+    bookCard.className = "book-card";
+    bookCard.setAttribute("data-idx", myLibrary.indexOf(book));
 
-    let book = { title, author, pages, status };
-    // addBookToLibrary(book);
-    myLibrary.push(book);
-    saveLocal(myLibrary);
-    clearBooks();
-    loadBooks();
-  }
-}
+    let bookTitle = document.createElement("h1");
+    bookTitle.textContent = book.title;
 
-setMyLibrary();
+    let bookDescription = document.createElement("p");
+    bookDescription.textContent = book.author;
+    bookDescription.className = "description";
 
-btnAddNewBook.addEventListener("click", openForm);
+    let bookPages = document.createElement("p");
+    bookPages.textContent = "Has " + book.pages + " pages.";
+    bookPages.className = "pages";
 
-function openForm() {
-  if (newBookForm.style.display === "flex") {
-    newBookForm.style.display = "none";
-  } else {
-    newBookForm.style.display = "flex";
-  }
-}
+    let readStatus = document.createElement("p");
+    if (book.status === true) {
+      readStatus.textContent = "You have read this book.";
+    } else {
+      readStatus.textContent = "You have not read this book.";
+    }
+    readStatus.className = "read-status-text";
 
-function addNewBook() {
-  if (newBookForm.style.display === "flex") {
-    newBookForm.style.display = "none";
-  } else {
-    newBookForm.style.display = "flex";
-  }
-}
+    let changeReadStatusBtn = document.createElement("button");
+    changeReadStatusBtn.className = "read-switch-btn";
 
-function saveLocal(myLibrary) {
-  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
-}
+    if (book.status === true) {
+      changeReadStatusBtn.textContent = "Unread";
+    } else {
+      changeReadStatusBtn.textContent = "Read";
+    }
+    changeReadStatusBtn.addEventListener("click", switchReadStatus);
 
+    let removeBtn = document.createElement("button");
+    removeBtn.className = "remove-btn";
+    removeBtn.textContent = "Remove Book";
+    removeBtn.addEventListener("click", removeCard);
 
-function clearBooks() {
-  container = document.querySelector('.books-container');
-  while (container.firstChild) {
-    container.removeChild(container.firstChild);
-  }
-}
-
-function loadBooks() {
-  myLibrary.forEach((book) => {
-    addBookToLibrary(book)
-  }
-  )
-}
-
-function addBookToLibrary(book) {
-  // do stuff here
-  let bookCard = document.createElement("div");
-  bookCard.className = "book-card";
-  bookCard.setAttribute("data-idx", myLibrary.indexOf(book));
-
-  let bookTitle = document.createElement("h1");
-  bookTitle.textContent = book.title;
-
-  let bookDescription = document.createElement("p");
-  bookDescription.textContent = book.author;
-  bookDescription.className = "description";
-
-  let bookPages = document.createElement("p");
-  bookPages.textContent = "Has " + book.pages + " pages.";
-  bookPages.className = "pages";
-
-  let readStatus = document.createElement("p");
-  if (book.status === true) {
-    readStatus.textContent = "You have read this book.";
-  } else {
-    readStatus.textContent = "You have not read this book.";
-  }
-  readStatus.className = "read-status-text";
-
-  let changeReadStatusBtn = document.createElement("button");
-  changeReadStatusBtn.className = "read-switch-btn";
-
-  if (book.status === true) {
-    changeReadStatusBtn.textContent = "Unread";
-  } else {
-    changeReadStatusBtn.textContent = "Read";
-  }
-  changeReadStatusBtn.addEventListener("click", switchReadStatus);
-
-  let removeBtn = document.createElement("button");
-  removeBtn.className = "remove-btn";
-  removeBtn.textContent = "Remove Book";
-  removeBtn.addEventListener("click", removeCard);
-
-  bookCard.append(
-    bookTitle,
-    bookDescription,
-    bookPages,
-    readStatus,
-    changeReadStatusBtn,
-    removeBtn
-  );
-  booksContainer.appendChild(bookCard);
-}
-
-function removeCard(e) {
-  e.target.parentNode.remove();
-  bookIndex = e.target.parentNode.getAttribute("data-idx");
-  myLibrary.splice(bookIndex, 1);
-  saveLocal(myLibrary);
-  clearBooks();
-  loadBooks();
-}
-
-function switchReadStatus(e) {
-  // switch button and read status for a book
-  clearBooks();
-  loadBooks();
-  bookIndex = e.target.parentNode.getAttribute("data-idx");
-  book = myLibrary[bookIndex];
-  let text = e.target.parentNode.querySelector(".read-status-text");
-
-  if (book.status === true) {
-    book.status = false;
-    e.target.textContent = "Read";
-    text.textContent = "You have not read this book.";
-  } else {
-    book.status = true;
-    e.target.textContent = "Unread";
-    text.textContent = "You have read this book.";
+    bookCard.append(
+      bookTitle,
+      bookDescription,
+      bookPages,
+      readStatus,
+      changeReadStatusBtn,
+      removeBtn
+    );
+    booksContainer.appendChild(bookCard);
   }
 
-  saveLocal(myLibrary);
-  clearBooks();
-  loadBooks(myLibrary);
+  function removeCard(e) {
+    e.target.parentNode.remove();
+    bookIndex = e.target.parentNode.getAttribute("data-idx");
+    myLibrary.splice(bookIndex, 1);
+    libraryController.saveLocal(myLibrary);
+    libraryController.clearBooks();
+    libraryController.loadBooks(myLibrary);
+  }
 
+  function switchReadStatus(e) {
+    // switch button and read status for a book
+    libraryController.clearBooks();
+    libraryController.loadBooks(myLibrary);
+    bookIndex = e.target.parentNode.getAttribute("data-idx");
+    book = myLibrary[bookIndex];
+    let text = e.target.parentNode.querySelector(".read-status-text");
+
+    if (book.status === true) {
+      book.status = false;
+      e.target.textContent = "Read";
+      text.textContent = "You have not read this book.";
+    } else {
+      book.status = true;
+      e.target.textContent = "Unread";
+      text.textContent = "You have read this book.";
+    }
+
+    libraryController.saveLocal(myLibrary);
+    libraryController.clearBooks();
+    libraryController.loadBooks(myLibrary);
+  }
+
+  return { addBookToLibrary };
+})();
+
+const myModule = (() =>{
+  
+  const setMyLibrary = () => {
+    let myLibrary = '';
+    if (localStorage.getItem("myLibrary") !== null) {
+      myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+      } else {
+      myLibrary = [];
+      }
+    return myLibrary;
+  }
+  
+  const loadBooks = (library) => {
+      library.forEach((book) => {
+      const mySecModuleLocal = mySecModule;
+      mySecModuleLocal.addBookToLibrary(book);
+    });
+  }
+  
+  const clearBooks = () => {
+    container = document.querySelector(".books-container");
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+  }
+  
+  const createNewBook = () => {
+    if (
+      bookTitleInput.value == "" ||
+      bookAuthorInput.value == "" ||
+      pagesInput.value == ""
+    ) {
+      alert("You must complete all fields.");
+    } else {
+      let title = bookTitleInput.value;
+      let author = bookAuthorInput.value;
+      let pages = pagesInput.value;
+      let status = readStatusInput.checked;
+      let book = { title, author, pages, status };
+      myLibrary.push(book);
+      saveLocal(myLibrary);
+      clearBooks();
+      loadBooks(myLibrary);
+    }
+  }
+  
+  const openForm = () => {
+    if (newBookForm.style.display === "flex") {
+      newBookForm.style.display = "none";
+    } else {
+      newBookForm.style.display = "flex";
+    }
+  }
+  
+  const saveLocal = (myLibrary) => {
+    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+  }
+
+  return { setMyLibrary, loadBooks, clearBooks, createNewBook, openForm, saveLocal };
+  
+})();
+// End of function module
+
+const libraryController = myModule;
+let myLibrary = libraryController.setMyLibrary();
+
+// Load books if it's something on localStorage
+if (myLibrary !== '') {
+  libraryController.loadBooks(myLibrary);
 }
 
-// myLibrary.forEach((book) => addBookToLibrary(book));
+// Add listeners for main add book button
+buttonInput.addEventListener("click", libraryController.createNewBook);
+btnAddNewBook.addEventListener("click", libraryController.openForm);
